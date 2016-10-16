627d4a3c34bf47d9d858e5dfc445a69bcaf123b2
Jose Daniel <velazcojd@gmail.com>
2016-10-16 17:14:36 -0500

    Temp/Moist Sensor Test Files
    Included the test files for the Temp/Moist (SHT10) sensor.

---
+++ TestFiles/TempMoist/TempMoist.ino
@@ -0,0 +1,41 @@
+/*   TempMoist.ino
+ *    This file is used to test the Temperature/Moisture sensor (SHT10). It is a copy of a test file 
+ *    included in the sht1x library. You need to add the library included in this
+ *    directory to make it work.
+ */
+
+#include <SHT1x.h>
+
+// Specify data and clock connections and instantiate SHT1x object
+#define dataPin  10
+#define clockPin 11
+SHT1x sht1x(dataPin, clockPin);
+
+void setup()
+{
+   Serial.begin(38400); // Open serial connection to report values to host
+   Serial.println("Starting up");
+}
+
+void loop()
+{
+  float temp_c;
+  float temp_f;
+  float humidity;
+
+  // Read values from the sensor
+  temp_c = sht1x.readTemperatureC();
+  temp_f = sht1x.readTemperatureF();
+  humidity = sht1x.readHumidity();
+
+  // Print the values to the serial port
+  Serial.print("Temperature: ");
+  Serial.print(temp_c, DEC);
+  Serial.print("C / ");
+  Serial.print(temp_f, DEC);
+  Serial.print("F. Humidity: ");
+  Serial.print(humidity);
+  Serial.println("%");
+
+  delay(2000);
+}
