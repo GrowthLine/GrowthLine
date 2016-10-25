640ecd91769e0cebee01ad8921c1cdb184e52e37
Jose Daniel <velazcojd@gmail.com>
2016-10-25 16:14:56 -0500

    Added main program file
    Added the main program file and some few changes

---
+++ GrowthLine/GrowthLine.ino
@@ -0,0 +1,49 @@
+// Libraries needed for Light Sensor
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+#include <pgmspace.h>
+// Libraries needed for Temperature/Moisture Sensor (SHT10)
+#include <SHT1x.h>
+
+
+#define READY_STATE = 0;
+#define WARMUP_STATE = 1;
+#define READ_STATE = 2;
+#define SAVE_STATE = 3;
+#define MENU_STATE = 4;
+#define CALIBRATE_STATE = 5;
+#define SHUTDOWN_STATE = 6;
+
+Adafruit_TSL2561_Unified luxSensor = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345); // Lux Sensor
+int deviceState = READY_STATE;
+
+
+void setup() {
+  Serial.begin(9600);
+  /* Configure Lux Sensor */
+  luxSensor.enableAutoRange(true);    // switches automatically between the different gains
+  luxSensor.setIntegrationTime(TSL2561_INTEGRATIONTIME_420MS);  // slowest but most accurate
+
+}
+
+void loop() {
+  switch (deviceState) {
+    case READY_STATE:
+      break;
+    case WARMUP_STATE:
+      break;
+    case READ_STATE:
+      break;
+    case SAVE_STATE:
+      break;
+    case MENU_STATE:
+      break;
+    case CALIBRATE_STATE:
+      break;
+    case SHUTDOWN_STATE:
+      break;
+    default:
+      break;
+  }
+
+}
