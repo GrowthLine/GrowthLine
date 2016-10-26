7d9872e282fb50c7b2d6aba0a73298fa13d86c46
Jose Daniel <velazcojd@gmail.com>
2016-10-26 13:52:51 -0500

    Fixed Bugs Related to Header File
    Some things shouldn't be in the header file, so I moved to main program.
    Also, the display functions are not working.

--- GrowthLine/GrowthLine.ino
+++ GrowthLine/GrowthLine.ino
@@ -1,14 +1,28 @@
-#include <GrowthLine.h>
-Adafruit_TSL2561_Unified luxSensor = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345); // Lux Sensor
-int deviceState = READY_STATE;
+#include "GrowthLine.h"
 
+// Libraries needed for Light Sensor
+#include <Adafruit_Sensor.h>
+#include <Adafruit_TSL2561_U.h>
+#include <pgmspace.h>
+// Libraries needed for Temperature/Moisture Sensor (SHT10)
+#include <SHT1x.h>
+
+// Libraries needed for Touch LCD
+#include <Adafruit_ILI9341.h>
+
+// Variable Declarations
+Adafruit_TSL2561_Unified luxSensor = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345); // Lux Sensorint
+int deviceState;        // State of device
 
 void setup() {
   Serial.begin(9600);
   /* Configure Lux Sensor */
+  //luxSensor = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345); // Lux Sensor
   luxSensor.enableAutoRange(true);    // switches automatically between the different gains
-  luxSensor.setIntegrationTime(TSL2561_INTEGRATIONTIME_420MS);  // slowest but most accurate
+  luxSensor.setIntegrationTime(TSL2561_INTEGRATIONTIME_402MS);  // slowest but most accurate
 
+  /* Initialize variables */
+  deviceState = READY_STATE;
 }
 
 void loop() {
@@ -31,18 +45,16 @@ void loop() {
     default:
       break;
   }
-  
-  void mainMenu() {
-    fillRect( 20, 20,120, 80, ILI9341_GREEN );
-    fillRect(170, 20,120, 80, ILI9341_YELLOW);
-    fillRect( 20,140,120, 80, ILI9341_ORANGE);
-    setCursor( 50, 50);
-    setTextColor( ILI9341_BLACK, ILI9341_GREEN);
-    
-  }
-  
-  void warmUpScreen() {
-    fillRect( 20, 20,120, 80, ILI9341_YELLOW);
-  }
+}
 
+void mainMenu() {
+  fillRect( 20, 20, 120, 80, ILI9341_GREEN );
+  fillRect(170, 20, 120, 80, ILI9341_YELLOW);
+  fillRect( 20, 140, 120, 80, ILI9341_ORANGE);
+  setCursor( 50, 50);
+  setTextColor( ILI9341_BLACK, ILI9341_GREEN);
+}
+
+void warmUpScreen() {
+  fillRect( 20, 20, 120, 80, ILI9341_YELLOW);
 }
