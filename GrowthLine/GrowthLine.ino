da36c097c85bd997ac6236097e8fd73c7c82495e
AndrewLeverkuhn <vic20.glf09@gmail.com>
2016-10-29 16:19:33 -0500

    Implement main menu
    ... or most of it.

--- GrowthLine/GrowthLine.ino
+++ GrowthLine/GrowthLine.ino
@@ -54,16 +54,65 @@ void loop() {
   }
 }
 
+/**
+ *  Tries to grab a point, returns null if none present.
+ **/
+TS_Point getTouch() {
+  // If the touchbuffer is empty, then return
+  if (ts.bufferEmpty()) {
+    return null;
+  }
+  // Grab the next touchpoint from the buffer
+  TS_Point p = ts.getPoint();
+  
+  // Scale the coordinates
+  p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+  
+  // Return the value
+  return p;
+}
 
-void mainMenu() {
+void draw_MainMenu() {
   tft.fillRect( 20, 20, 120, 80, ILI9341_GREEN );
   tft.fillRect(170, 20, 120, 80, ILI9341_YELLOW);
   tft.fillRect( 20, 140, 120, 80, ILI9341_ORANGE);
+  // Write text on button 1
   tft.setCursor( 50, 50);
-  tft.setTextColor( ILI9341_BLACK, ILI9341_GREEN);
+  tft.setTextColor( ILI9341_BLACK, ILI9341_GREEN );
+  tft.println("Read");
+  // Write text on button 2
+  tft.setCursor(200, 50);
+  tft.setTextColor( ILI9341_BLACK, ILI9341_YELLOW);
+  tft.println("Menu");
+  // Write text on button 3
+  tft.setCursor( 50,170);
+  tft.setTextColor( ILI9341_BLACK, ILI9341_ORANGE);
+  tft.println("Save");
 }
 
-void warmUpScreen() {
+int check_MainMenu() {
+  TS_Point p = getTouch();
+  if (p==null) {
+    return;
+  }
+  // First button check: 'Read'
+  if ((p.x >=  20 && p.x <= 140) && (p.y >=  20 && p.y <= 100)) {
+    return 1;
+  }
+  // Second button check: 'Menu'
+  if ((p.x >= 170 && p.x <= 290) && (p.y >=  20 && p.y <= 100)) {
+    return 2;
+  }
+  // Third button check: 'Save'
+  if ((p.x >=  20 && p.x <= 140) && (p.y >= 140 && p.y <= 260)) {
+    return 3;
+  }
+  return 0;
+   
+}
+
+void draw_WarmUpScreen() {
   tft.fillRect( 20, 20, 120, 80, ILI9341_YELLOW);
 }
 
