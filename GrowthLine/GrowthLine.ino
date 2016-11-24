65fe8eeffc985bc928755e1b68f00929500893cc
RosalesM <RosalesM.6900@gmail.com>
2016-11-23 22:08:38 -0600

    SD Log Exist Check
    Added method to check if log file exists

--- GrowthLine/GrowthLine.ino
+++ GrowthLine/GrowthLine.ino
@@ -554,6 +554,7 @@ void draw_LogScreen(String in_array[]) {
   }
 }
 
+//Overwrite "settings.txt" when changes are made
 void newSettings (){
   if (saveEnable){
       SD.remove("settings.txt");
@@ -562,6 +563,32 @@ void newSettings (){
       settingsFile.println("LogFile=" + String(logFileNumber));
       settingsFile.println("Reading=" + String(readingNumber));
       settingsFile.close();
+  }
+}
+
+//Save log to SD card
+void saveLog(){
+  String logFileName = "log" + String(logFileNumber) + ".txt";
+  if(SD.exists(logFileName)){
+    File logFile = SD.open(logFileName, FILE_WRITE);
+    logFile.println(String(readingNumber)+ "," + readings.peek().toString());
+    logFile.close();
+  }
+  else{
+    Serial.println("Log File Not Found!");
+  }
+}
+
+void checkLogExists(){
+  String logFileName = "log" + String(logFileNumber) + ".txt";
+  String fileHeader = "Reading Number,Soil Temperature,Soil Moisture,Soil pH,Air Temperature,Air Humidity,Lux";
+  if(!SD.exists(logFileName)){
+    File logFile = SD.open(logFileName, FILE_WRITE);
+    logFile.println(fileHeader);
+    logFile.close();
+  }
+  else{
+    Serial.println("Log File Found!");
   }
 }
 
